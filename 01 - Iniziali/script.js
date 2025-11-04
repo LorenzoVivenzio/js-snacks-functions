a0114936ac05f307c90aa94273bab4bb47252a85
LorenzoVivenzio <lorenzovivenzio988@hotmail.com>
2025-11-04 16:32:10 +0100

    feat: esercizi con iniziali

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -1,13 +1,24 @@
 /* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
+// console.log(names)
 
 
 // Dichiara la funzione qui.
+function inizialeNome(names){
+    let primaLettera = []
 
+    for(let i = 0; i < names.length; i++){
+        const curPrimaLettera = names[i]
+        primaLettera.push(curPrimaLettera[0])
+
+    }
+    return primaLettera
+
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(inizialeNome(names))
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
